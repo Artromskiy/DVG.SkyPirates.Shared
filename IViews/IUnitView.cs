b38175bf5f159437a883806cc615b594aa4054b1
Artromskiy <artromskiy@gmail.com>
2025-05-07 00:39:54 +0300

    Ids added to shared

--- IViews/IUnitView.cs
+++ IViews/IUnitView.cs
@@ -1,12 +1,11 @@
 using DVG.Core;
-using DVG.MathsOld;
 
 namespace DVG.SkyPirates.Shared.IViews
 {
     public interface IUnitView : IView
     {
         public float2 Velocity { set; }
-        public angle Rotation { set; }
+        //public angle Rotation { set; }
 
         public float3 Position { get; }
     }
