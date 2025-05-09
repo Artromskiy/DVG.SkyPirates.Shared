6579dab950a937998355ccf1c1666bb714e93328
Artromskiy <artromskiy@gmail.com>
2025-05-09 20:18:15 +0300

    wip

--- IServices/IMessageSendService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DVG.SkyPirates.Server.IServices
-{
-    public interface IMessageSendService
-    {
-        public void SendToAll<T>(T data) where T : unmanaged;
-        public void SendTo<T>(T data, int clientId) where T : unmanaged;
-    }
-}
