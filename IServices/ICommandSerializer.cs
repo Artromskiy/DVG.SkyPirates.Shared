77ef41745178a9d34b77e42fa736562dfb1690fc
Artromskiy <artromskiy@gmail.com>
2025-05-09 23:28:05 +0300

    Command Serializer

---
+++ IServices/ICommandSerializer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DVG.SkyPirates.Shared.IServices
+{
+    public interface ICommandSerializer
+    {
+        ReadOnlySpan<byte> Serialize<T>(ref T data) where T : unmanaged;
+        T Deserialize<T>(ReadOnlySpan<byte> data) where T : unmanaged;
+    }
+}
---
+++ Services/CommandSerializer.cs
@@ -0,0 +1,21 @@
+﻿using DVG.SkyPirates.Shared.IServices;
+using System;
+using System.Runtime.InteropServices;
+
+namespace DVG.SkyPirates.Shared.Services
+{
+    public class CommandSerializer : ICommandSerializer
+    {
+        public ReadOnlySpan<byte> Serialize<T>(ref T data) where T : unmanaged
+        {
+            var span = MemoryMarshal.CreateReadOnlySpan(ref data, 1);
+            ReadOnlySpan<byte> spanBytes = MemoryMarshal.AsBytes(span);
+            return spanBytes;
+        }
+
+        public T Deserialize<T>(ReadOnlySpan<byte> data) where T : unmanaged
+        {
+            return MemoryMarshal.Read<T>(data);
+        }
+    }
+}
