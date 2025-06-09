62d94ab7301362026bfe98a44dc4ec074551e791
Artromskiy <artromskiy@gmail.com>
2025-06-10 00:07:19 +0300

    SquadFactory

---
+++ Factories/SquadFactory.cs
@@ -0,0 +1,26 @@
+﻿using DVG.Core;
+using DVG.SkyPirates.Shared.Commands;
+using DVG.SkyPirates.Shared.IFactories;
+using DVG.SkyPirates.Shared.Models;
+using DVG.SkyPirates.Shared.Presenters;
+
+namespace DVG.SkyPirates.Shared.Factories
+{
+    public class SquadFactory : ISquadFactory
+    {
+        private readonly IPathFactory<PackedCirclesModel> _circlesModelFactory;
+
+        public SquadFactory(IPathFactory<PackedCirclesModel> circlesModelFactory)
+        {
+            _circlesModelFactory = circlesModelFactory;
+        }
+
+        public SquadPm Create(Command<SpawnSquad> parameters)
+        {
+            var squad = new SquadPm(_circlesModelFactory);
+            return squad;
+        }
+
+        public void Dispose(SquadPm instance) { }
+    }
+}
