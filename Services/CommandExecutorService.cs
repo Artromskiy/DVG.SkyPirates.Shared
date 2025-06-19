09751e66967ea6c1847f7ff77940ce5b4181bea7
Artromskiy <artromskiy@gmail.com>
2025-06-19 14:08:52 +0300

    IEnumerable to array to remove runtime resoulution

--- Services/CommandExecutorService.cs
+++ Services/CommandExecutorService.cs
@@ -2,21 +2,21 @@
 using DVG.SkyPirates.Shared.IServices;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DVG.SkyPirates.Shared.Services
 {
     public class CommandExecutorService : ICommandExecutorService
     {
-        private readonly IEnumerable<ICommandExecutor> _executors;
+        private readonly ICommandExecutor[] _executors;
 
         public CommandExecutorService(IEnumerable<ICommandExecutor> executors)
         {
-            _executors = executors;
+            _executors = executors.ToArray();
         }
 
         public void Execute<T>(Command<T> cmd) where T : ICommandData
         {
-            Console.WriteLine(cmd.ClientId);
             foreach (var executor in _executors)
                 if(executor is ICommandExecutor<T> concreteExecutor)
                     concreteExecutor?.Execute(cmd);
