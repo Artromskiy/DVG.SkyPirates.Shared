74494cc5eb9b093aedbf6c1567ee552da33ad72a
Artromskiy <artromskiy@gmail.com>
2025-08-12 18:02:01 +0300

    wip

--- IViews/IUnitView.cs
+++ IViews/IUnitView.cs
@@ -1,20 +1,7 @@
-#region Reals
-using real = System.Single;
-using real2 = DVG.float2;
-using real3 = DVG.float3;
-using real4 = DVG.float4;
-#endregion
-
 using DVG.Core;
 using DVG.SkyPirates.Shared.IViewModels;
 
 namespace DVG.SkyPirates.Shared.IViews
 {
-    public interface IUnitView: IView<IUnitVM>
-    {
-        real3 Position { get; }
-        real Rotation { get; }
-        real PreAttack { get; }
-        real PostAttack { get; }
-    }
+    public interface IUnitView : IView<IUnitVM> { }
 }
