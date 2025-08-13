de0f2fa13edd1823cad4dc487cc01187b03c1ce9
Artromskiy <artromskiy@gmail.com>
2025-08-13 15:46:45 +0300

    wip

--- IFactories/IUnitViewFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿using DVG.Core;
-using DVG.SkyPirates.Shared.Commands;
-using DVG.SkyPirates.Shared.IViews;
-
-namespace DVG.SkyPirates.Shared.IFactories
-{
-    public interface IUnitViewFactory : IFactory<IUnitView, SpawnUnit> { }
-}
