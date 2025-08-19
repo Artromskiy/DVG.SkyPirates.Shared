30211e935eb86042696b1eeb9f66b9df5a0b51e3
Artromskiy <artromskiy@gmail.com>
2025-08-19 04:29:45 +0300

    wip

--- Factories/SquadFactory.cs
+++ Factories/SquadFactory.cs
@@ -1,25 +1,19 @@
 ﻿using DVG.Core;
 using DVG.SkyPirates.Shared.Commands;
 using DVG.SkyPirates.Shared.IFactories;
-using DVG.SkyPirates.Shared.IServices;
-using DVG.SkyPirates.Shared.Configs;
 using DVG.SkyPirates.Shared.Entities;
 using System.Collections.Generic;
 using Arch.Core;
 using DVG.SkyPirates.Shared.Components;
-using Arch.Core.Extensions;
 
 namespace DVG.SkyPirates.Shared.Factories
 {
     public class SquadFactory : ISquadFactory
     {
-        private readonly IPathFactory<PackedCirclesConfig> _circlesModelFactory;
-
         private readonly Dictionary<int, Entity> _squadsCache = new Dictionary<int, Entity>();
 
-        public SquadFactory(IPathFactory<PackedCirclesConfig> circlesModelFactory)
+        public SquadFactory()
         {
-            _circlesModelFactory = circlesModelFactory;
         }
 
         public Entity Create(Command<SpawnSquadCommand> cmd)
---
+++ Factories/UnitFactory.cs
@@ -0,0 +1,42 @@
+﻿using DVG.Core;
+using DVG.SkyPirates.Shared.Commands;
+using DVG.SkyPirates.Shared.IFactories;
+using DVG.SkyPirates.Shared.Entities;
+using System.Collections.Generic;
+using Arch.Core;
+using DVG.SkyPirates.Shared.Components;
+
+namespace DVG.SkyPirates.Shared.Factories
+{
+    public class UnitFactory : IUnitFactory
+    {
+        private readonly IUnitConfigFactory _unitConfigFactory;
+
+        private readonly Dictionary<int, Entity> _unitsCache = new Dictionary<int, Entity>();
+
+        public UnitFactory(IUnitConfigFactory unitConfigFactory)
+        {
+            _unitConfigFactory = unitConfigFactory;
+        }
+
+        public Entity Create(Command<SpawnUnitCommand> cmd)
+        {
+            if (_unitsCache.TryGetValue(cmd.EntityId, out var unit))
+                return unit;
+
+            var config = _unitConfigFactory.Create(cmd.Data);
+
+            _unitsCache[cmd.EntityId] = unit = EntityIds.Get(cmd.EntityId);
+
+            unit.Add<
+                Unit,
+                Position,
+                Rotation,
+                Health,
+                Team,
+                Fixation>();
+
+            return unit;
+        }
+    }
+}
