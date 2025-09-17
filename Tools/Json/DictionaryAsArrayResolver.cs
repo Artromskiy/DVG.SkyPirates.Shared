eaf0107c423ce0ecedb41c307c6df36ebae54a61
Artromskiy <artromskiy@gmail.com>
2025-09-17 03:16:45 +0300

    wip

---
+++ Tools/Json/DictionaryAsArrayResolver.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+
+namespace DVG.SkyPirates.Shared.Tools.Json
+{
+    public class DictionaryAsArrayResolver : DefaultContractResolver
+    {
+        protected override JsonContract CreateContract(Type objectType)
+        {
+            if (IsDictionary(objectType))
+            {
+                JsonArrayContract contract = base.CreateArrayContract(objectType);
+                contract.OverrideCreator = (args) => CreateInstance(objectType);
+                return contract;
+            }
+
+            return base.CreateContract(objectType);
+        }
+
+        private object CreateInstance(Type objectType)
+        {
+            Type dictionaryType = typeof(Dictionary<,>).MakeGenericType(objectType.GetGenericArguments());
+            return Activator.CreateInstance(dictionaryType);
+        }
+
+        private static bool IsDictionary(Type objectType)
+        {
+            if (objectType.IsGenericType && objectType.GetGenericTypeDefinition() == typeof(IDictionary<,>))
+            {
+                return true;
+            }
+
+            if (objectType.GetInterface(typeof(IDictionary<,>).Name) != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+}
---
+++ Tools/Json/Serialization.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace DVG.SkyPirates.Shared.Tools.Json
+{
+    public static class Serialization
+    {
+        private static readonly JsonSerializerSettings? _settings;
+        static Serialization()
+        {
+            _settings = JsonConvert.DefaultSettings?.Invoke();
+            if (_settings != null)
+            {
+                _settings.ContractResolver = new DictionaryAsArrayResolver();
+            }
+        }
+
+        public static string Serialize<T>(T obj)
+        {
+            return JsonConvert.SerializeObject(obj, _settings);
+        }
+
+        public static T Deserialize<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json, _settings);
+        }
+    }
+}
