158721b7205883253ab91135e108a82f7d9fa2fa
rumtery <rumtery@gmail.com>
2026-02-13 02:28:11 +0300

    GoodsCollectorRadius and GoodsCollectorSystem

---
+++ Components/Runtime/GoodsCollectorRadius.cs
@@ -0,0 +1,13 @@
+﻿using DVG.Components.Attributes;
+using DVG.NewType;
+using System;
+
+namespace DVG.SkyPirates.Shared.Components.Runtime
+{
+    [Component(true)]
+    public struct GoodsCollectorRadius : INewType
+    {
+        [Obsolete("Use INewType implicits")]
+        public fix Value;
+    }
+}
