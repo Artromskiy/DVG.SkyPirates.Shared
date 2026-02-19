e539c426b121d2d0954d8b47b5c343b536da5468
rumtery <rumtery@gmail.com>
2026-02-20 00:33:43 +0300

    TickRate 10 => 16

--- Constants.cs
+++ Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public const int TicksPerSecond = 10;
+        public const int TicksPerSecond = 16;
         public const int HistoryTicks = 60_0; // 1 minute ?
 
         public static readonly fix TickTime = fix.One / TicksPerSecond;
