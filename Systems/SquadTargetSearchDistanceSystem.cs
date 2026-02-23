842ca453f0d6d2f055a1de3256841f3fa74dc850
rumtery <rumtery@gmail.com>
2026-02-23 19:47:14 +0300

    Changed squad range logic

--- Systems/SquadTargetSearchDistanceSystem.cs
+++ Systems/SquadTargetSearchDistanceSystem.cs
@@ -1,19 +1,22 @@
 ﻿using Arch.Core;
+using DVG.Components;
 using DVG.SkyPirates.Shared.Components.Config;
 using DVG.SkyPirates.Shared.Components.Framed;
 using DVG.SkyPirates.Shared.Components.Runtime;
 using DVG.SkyPirates.Shared.IFactories;
 using DVG.SkyPirates.Shared.IServices.TickableExecutors;
+using System.Collections.Generic;
 
 namespace DVG.SkyPirates.Shared.Systems
 {
     public class SquadTargetSearchDistanceSystem : ITickableExecutor
     {
-        private const int BaseRange = 3;
-
+        private readonly QueryDescription _squadsMembers = new QueryDescription().
+            WithAll<SquadMember, ImpactDistance>();
         private readonly QueryDescription _squadsDesc = new QueryDescription().
             WithAll<Squad, SquadMemberCount, Fixation, TargetSearchDistance>().NotDisposing().NotDisabled();
 
+        private readonly Dictionary<int, fix> _rangePerSquad = new();
         private readonly IPackedCirclesFactory _packedCirclesFactory;
         private readonly World _world;
 
@@ -26,29 +29,38 @@ namespace DVG.SkyPirates.Shared.Systems
 
         public void Tick(int tick, fix deltaTime)
         {
-            var squadQuery = new ApplyRangeQuery(_packedCirclesFactory);
-            _world.InlineQuery<ApplyRangeQuery, Fixation, SquadMemberCount, TargetSearchDistance>(
+            _rangePerSquad.Clear();
+            _world.Query(in _squadsMembers, (ref SquadMember member, ref ImpactDistance impactDistance) =>
+            {
+                _rangePerSquad[member.SquadId] =
+                    Maths.Max(_rangePerSquad.GetValueOrDefault(member.SquadId), impactDistance);
+            });
+
+            var squadQuery = new ApplyRangeQuery(_packedCirclesFactory, _rangePerSquad);
+            _world.InlineQuery<ApplyRangeQuery, SyncId, Fixation, SquadMemberCount, TargetSearchDistance>(
                 _squadsDesc, ref squadQuery);
         }
 
         private readonly struct ApplyRangeQuery
-            : IForEach<Fixation, SquadMemberCount, TargetSearchDistance>
+            : IForEach<SyncId, Fixation, SquadMemberCount, TargetSearchDistance>
         {
             private readonly IPackedCirclesFactory _factory;
+            private readonly Dictionary<int, fix> _rangePerSquad;
 
-            public ApplyRangeQuery(IPackedCirclesFactory factory)
+            public ApplyRangeQuery(IPackedCirclesFactory factory, Dictionary<int, fix> rangePerSquad)
             {
                 _factory = factory;
+                _rangePerSquad = rangePerSquad;
             }
 
             public void Update(
+                ref SyncId syncId,
                 ref Fixation fixation,
                 ref SquadMemberCount memberCount,
                 ref TargetSearchDistance searchDistance)
             {
-                fix addRadius = memberCount == 0 ? 0 :
-                    _factory.Create(memberCount).Radius;
-                searchDistance = fixation ? 0 : BaseRange + addRadius;
+                fix addRadius = memberCount == 0 ? 0 : _factory.Create(memberCount).Radius;
+                searchDistance = fixation ? 0 : _rangePerSquad.GetValueOrDefault(syncId) + addRadius;
             }
         }
     }
