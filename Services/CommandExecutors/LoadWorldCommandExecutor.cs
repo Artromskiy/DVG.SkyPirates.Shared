1f24dcec1f16e475563aa834cbfc9f4e12285577
rumtery <rumtery@gmail.com>
2026-03-01 01:57:31 +0300

    LoadWorldCommandExecutor uses new SnapshotSystem

--- Services/CommandExecutors/LoadWorldCommandExecutor.cs
+++ Services/CommandExecutors/LoadWorldCommandExecutor.cs
@@ -1,6 +1,5 @@
 ﻿using DVG.Commands;
 using DVG.SkyPirates.Shared.Commands;
-using DVG.SkyPirates.Shared.IFactories;
 using DVG.SkyPirates.Shared.IServices;
 using DVG.SkyPirates.Shared.IServices.TickableExecutors;
 
@@ -8,18 +7,16 @@ namespace DVG.SkyPirates.Shared.Services.CommandExecutors
 {
     public class LoadWorldCommandExecutor : ICommandExecutor<LoadWorldCommand>
     {
-        private readonly IWorldDataFactory _worldDataFactory;
         private readonly IHistorySystem _historySystem;
 
-        public LoadWorldCommandExecutor(IWorldDataFactory worldDataFactory, IHistorySystem historySystem)
+        public LoadWorldCommandExecutor(IHistorySystem historySystem)
         {
-            _worldDataFactory = worldDataFactory;
             _historySystem = historySystem;
         }
 
         public void Execute(Command<LoadWorldCommand> cmd)
         {
-            _worldDataFactory.Extract(cmd.Data.WorldData);
+            _historySystem.ApplySnapshot(cmd.Data.WorldData);
             _historySystem.Save(cmd.Tick - 1);
         }
     }
