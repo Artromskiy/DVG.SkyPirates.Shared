fd030297b6fb3316810290fd86b10c4ec96da57b
rumtery <rumtery@gmail.com>
2026-03-01 18:57:43 +0300

    Back to old Document to Hash (ordering of dict)

--- Services/HashSumService.cs
+++ Services/HashSumService.cs
@@ -1,7 +1,11 @@
 ﻿using DVG.SkyPirates.Shared.IServices.TickableExecutors;
 using DVG.SkyPirates.Shared.Tools.Json;
+using System;
 using System.Buffers;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
@@ -39,9 +43,11 @@ namespace DVG.SkyPirates.Shared.Services
         {
             int targetTick = Maths.Max(0, tick - Constants.ValidTicksCount);
             var worldData = _historySystem.GetSnapshot(targetTick);
+            using var document = JsonSerializer.SerializeToDocument(worldData, _serializerOptions);
             _jsonWriter.Reset();
             _bufferWriter.Clear();
-            JsonSerializer.Serialize(_jsonWriter, worldData, _serializerOptions);
+            WriteOrdered(_jsonWriter, document.RootElement);
+            _jsonWriter.Flush();
             if (!_hashing.TryComputeHash(_bufferWriter.WrittenSpan, _hashResult, out _))
                 Debug.Assert(false, "Failed to get HashSum");
 
@@ -52,6 +58,55 @@ namespace DVG.SkyPirates.Shared.Services
             _sHash = _sBuilder.ToString();
 
             Debug.WriteLine($"Tick: {targetTick}. Hash: {_sHash}");
+        }
+
+        private void WriteOrdered(Utf8JsonWriter jsonWriter, JsonElement element)
+        {
+            if (element.ValueKind is JsonValueKind.Object)
+            {
+                var properties = element.EnumerateObject().OrderBy(p => p.Name, StringComparer.Ordinal);
+                var count = element.GetPropertyCount();
+                var i = count;
+                var array = ArrayPool<JsonProperty>.Shared.Rent(count);
+                foreach (var item in element.EnumerateObject())
+                    array[--i] = item;
+                Array.Sort(array, JsonPropertyComparer.Default);
+
+                jsonWriter.WriteStartObject();
+                for (; i < count; i++)
+                {
+                    jsonWriter.WritePropertyName(JsonMarshal.GetRawUtf8PropertyName(array[i]));
+                    WriteOrdered(jsonWriter, array[i].Value);
+                }
+                jsonWriter.WriteEndObject();
+                ArrayPool<JsonProperty>.Shared.Return(array);
+            }
+
+            else if (element.ValueKind is JsonValueKind.Array)
+            {
+                jsonWriter.WriteStartArray();
+                foreach (var item in element.EnumerateArray())
+                    WriteOrdered(jsonWriter, item);
+                jsonWriter.WriteEndArray();
+            }
+
+            else
+            {
+                element.WriteTo(jsonWriter);
+            }
+        }
+
+
+        private class JsonPropertyComparer : IComparer<JsonProperty>
+        {
+            public static JsonPropertyComparer Default { get; } = new();
+
+            public int Compare(JsonProperty x, JsonProperty y)
+            {
+                var xRaw = JsonMarshal.GetRawUtf8PropertyName(x);
+                var yRaw = JsonMarshal.GetRawUtf8PropertyName(y);
+                return xRaw.SequenceCompareTo(yRaw);
+            }
         }
     }
 }
